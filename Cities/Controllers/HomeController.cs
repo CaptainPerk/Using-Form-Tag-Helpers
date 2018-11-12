27c2992309e2046a5502f16246deb8c667c3723a
Jon Perkins <captainperk81@gmail.com>
2018-11-12 07:18:46 -0500

    Importing the Solution

---
+++ Cities/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Cities.Models;
+using Cities.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cities.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public HomeController(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        public ViewResult Index() => View(_cityRepository.Cities);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City newCity)
+        {
+            _cityRepository.AddCity(newCity);
+            return RedirectToAction("Index");
+        }
+    }
+}
